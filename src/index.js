2f5ccf70a56c8770c0ee5521836cc865a774ecfa
askarsheripbaev <sheripbaev1221@gmail.com>
2023-01-10 10:57:47 +0600

    change name

--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {BrowserRouter, Route, Switch} from "react-router-dom";
+import {HashRouter, Route, Switch} from "react-router-dom";
 import PostPage from "./pages/post";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <BrowserRouter>
+    <HashRouter>
         <Switch>
             <Route exact path='/'>
                 <App/>
@@ -17,7 +17,7 @@ root.render(
                 <PostPage />
             </Route>
         </Switch>
-    </BrowserRouter>
+    </HashRouter>
 );
 
 reportWebVitals();
